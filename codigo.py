072410d752d46060ea76e5361a6d4f33c2ce365f
Ana Freitas <ana.freitaas16@gmail.com>
2024-03-06 22:31:24 -0300

    add database

--- codigo.py
+++ codigo.py
@@ -30,7 +30,44 @@ time.sleep(3)
 
 # Passo 3: Importar a base de dados
 tabela = pandas.read_csv("produtos.csv")
-print(tabela)
+# print(tabela)
+
+# Passo 5: Repetir o processo até o final da base de dados
+for linha in tabela.index:
+
+    codigo = tabela.loc[linha, "codigo"]
+    marca = tabela.loc[linha, "marca"]
+    tipo = tabela.loc[linha, "tipo"]
+    categoria = str(tabela.loc[linha, "categoria"])
+    preco = str(tabela.loc[linha, "preco_unitario"])
+    custo = str(tabela.loc[linha, "custo"])
+    obs = tabela.loc[linha, "obs"]
 
 # Passo 4: Cadastrar um produto
-# Passo 5: Repetir o processo até o final da base de dados
+    pyautogui.click(x=1015, y=297)
+
+    pyautogui.write(codigo)
+    pyautogui.press("tab")
+
+    pyautogui.write(marca)
+    pyautogui.press("tab")
+
+    pyautogui.write(tipo)
+    pyautogui.press("tab")
+
+    pyautogui.write(categoria)
+    pyautogui.press("tab")
+
+    pyautogui.write(preco)
+    pyautogui.press("tab")
+
+    pyautogui.write(custo)
+    pyautogui.press("tab")
+
+    if not pandas.isna(obs):
+        pyautogui.write(obs)
+        
+    pyautogui.press("tab")
+
+    pyautogui.press("enter")
+    pyautogui.scroll(5000)
